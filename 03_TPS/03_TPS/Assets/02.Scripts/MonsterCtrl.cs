4fbff2af1b55f38d2052a255fe2a0609e4df6327
kimkeon827 <111333313+kimkeon827@users.noreply.github.com>
2023-04-25 01:04:42 +0900

    0425 - 1
    게임매니저, 싱글톤 구현. 몬스터 사망시 몬스터풀로 다시 돌려놓았는데, hp를 100으로 해놔도 다시 살아놨을 때 State.DIE 상태여서 한번 죽은 개체는 살아나자마자 다시 죽는 버그 발생. DIE 상태 후 IDLE 상태로 돌려놓고 몬스터 풀로 이동

--- 03_TPS/03_TPS/Assets/02.Scripts/MonsterCtrl.cs
+++ 03_TPS/03_TPS/Assets/02.Scripts/MonsterCtrl.cs
@@ -41,12 +41,17 @@ public class MonsterCtrl : MonoBehaviour
     private GameObject bloodEffect;
 
     // 몬스터 생명 변수
-    private int hp = 100;
+    public int hp = 100;
 
     // 스크립트가 활성화될 때마다 호출되는 함수
     void OnEnable(){
         // 이벤트 발생 시 수행할 함수 연결
         PlayerCtrl.OnPlayerDie += this.OnPlayerDie;
+
+        // 몬스터의 상태를 체크하는 코루틴 함수 호출
+        StartCoroutine(CheckMonsterState());
+        // 상태에 따라 몬스터의 행동을 수행하는 코루틴 함수 호출
+        StartCoroutine(MonsterAction());
     }
 
     // 스크립트가 비활성화될 때마다 호출되는 함수
@@ -55,7 +60,7 @@ public class MonsterCtrl : MonoBehaviour
         PlayerCtrl.OnPlayerDie -= this.OnPlayerDie;
     }
 
-    void Start (){
+    void Awake (){
         // 몬스터의 Transform 할당
         monsterTr = GetComponent<Transform>();
 
@@ -71,10 +76,7 @@ public class MonsterCtrl : MonoBehaviour
         // BloodSprayEffect 프리팹 로드
         bloodEffect = Resources.Load<GameObject>("BloodSprayEffect");
 
-        // 몬스터의 상태를 체크하는 코루틴 함수 호출
-        StartCoroutine(CheckMonsterState());
-        // 상태에 따라 몬스터의 행동을 수행하는 코루틴 함수 호출
-        StartCoroutine(MonsterAction());
+        
     }
 
     // 일정한 간격으로 몬스터의 행동 상태를 체크
@@ -144,6 +146,22 @@ public class MonsterCtrl : MonoBehaviour
                     anim.SetTrigger(hashDie);
                     // 몬스터의 Collider 컴포넌트 비활성화
                     GetComponent<CapsuleCollider>().enabled = false;
+
+                    // 일정 시간 대기 후 오브젝트 풀링으로 환원
+                    yield return new WaitForSeconds(3.0f);
+
+                    // 사망 후 다시 사용할 때를 위해 hp 값 초기화
+                    hp = 100;
+                    isDie = false;
+
+                    // 사망 후 다시 사용하기위해 IDLE State로 변경
+                    this.state = State.IDLE;
+
+                    // 몬스터의 Collider 컴포넌트 활성화
+                    GetComponent<CapsuleCollider>().enabled = true;
+                    // 몬스터를 비활성화
+                    this.gameObject.SetActive(false);
+
                     break;
             }
             yield return new WaitForSeconds(0.3f);
