3cec5343ea0c801f783bf836ae6e8a5f104b7361
kimkeon827 <111333313+kimkeon827@users.noreply.github.com>
2023-04-28 06:10:02 +0900

    0428 - 1
    로비(ID, RoomID 입력 씬), 총알, 플레이어 사망, 리스폰 구현

--- 02_Network/AngryBot2Net/Assets/Scripts/Movement.cs
+++ 02_Network/AngryBot2Net/Assets/Scripts/Movement.cs
@@ -5,7 +5,7 @@ using Photon.Pun;
 using Photon.Realtime;
 using Cinemachine;
 
-public class Movement : MonoBehaviourPunCallbacks
+public class Movement : MonoBehaviourPunCallbacks, IPunObservable
 {
     // 컴포넌트 캐시 처리를 위한 변수
     private CharacterController controller;
@@ -26,6 +26,12 @@ public class Movement : MonoBehaviourPunCallbacks
 
     // 이동 속도
     public float moveSpeed = 10.0f;
+
+    // 수신된 위치와 회전값을 저장할 변수
+    private Vector3 receivePos;
+    private Quaternion receiveRot;
+    // 수신된 좌표로의 이동 및 회전 속도의 민감도
+    public float damping = 10.0f;
 
     void Start()
     {
@@ -55,6 +61,14 @@ public class Movement : MonoBehaviourPunCallbacks
         {
             Move();
             Turn();
+        }
+        else
+        {
+            // 수신된 좌표로 보간한 이동 처리
+            transform.position = Vector3.Lerp(transform.position, receivePos, Time.deltaTime * damping);
+
+            // 수신된 회전값으로 보간한 회전 처리
+            transform.rotation = Quaternion.Slerp(transform.rotation, receiveRot, Time.deltaTime * damping);
         }
     }
 
@@ -103,5 +117,19 @@ public class Movement : MonoBehaviourPunCallbacks
         lookDir.y = 0;
         // 주인공 캐릭터의 회전값 지정
         transform.localRotation = Quaternion.LookRotation(lookDir);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info){
+        // 자신의 로컬 캐릭터인 경우 자신의 데이터를 다른 네트워크 유저에게 송신
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            receivePos = (Vector3)stream.ReceiveNext();
+            receiveRot = (Quaternion)stream.ReceiveNext();
+        }
     }
 }
