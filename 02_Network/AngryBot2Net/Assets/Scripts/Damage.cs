57fd9f7000290a00e86214bb0935ce5bf9ec6198
kimkeon827 <111333313+kimkeon827@users.noreply.github.com>
2023-04-28 11:49:01 +0900

    0428 - 2
    게임 참여인원. 게임룸 이름. 사망한 인원. 죽인 인원 로그에 뜨도록 구현.

--- 02_Network/AngryBot2Net/Assets/Scripts/Damage.cs
+++ 02_Network/AngryBot2Net/Assets/Scripts/Damage.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Player = Photon.Realtime.Player;
 
-public class Damage : MonoBehaviour
+public class Damage : MonoBehaviourPunCallbacks
 {
     // 사망 후 투명 처리를 위한 MeshRenderer 컴포넌트의 배열
     private Renderer[] renderers;
@@ -19,6 +22,9 @@ public class Damage : MonoBehaviour
     private readonly int hashDie = Animator.StringToHash("Die");
     private readonly int hashRespawn = Animator.StringToHash("Respawn");
 
+    // GameManager 접근을 위한 변수
+    private GameManager gameManager;
+
     void Awake()
     {
         // 캐릭터 모델의 모든 Renderer 컴포넌트를 추출한 후 배열에 할당
@@ -28,20 +34,41 @@ public class Damage : MonoBehaviour
 
         // 현재 생명치를 초기 생명치로 초깃값 설정
         currHp = initHp;
+
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
-    void OnCollisionEnter(Collision coll){
+    void OnCollisionEnter(Collision coll)
+    {
         // 생명 수치가 0보다 크고 충돌체의 태그가 BULLET인 경우에 생명 수치를 차감
         if (currHp > 0 && coll.collider.CompareTag("BULLET"))
         {
             currHp -= 20;
             if (currHp <= 0)
             {
+                // 자신의 PhotonView일 때만 메시지를 출력
+                if (photonView.IsMine)
+                {
+                    // 총알의 ActorNumber를 추출
+                    var actorNo = coll.collider.GetComponent<Bullet>().actorNumber;
+                    // ActorNumber로 현재 룸에 입장한 플레이어를 추출
+                    Player lastShootPlayer = PhotonNetwork.CurrentRoom.GetPlayer(actorNo);
+
+                    // 메시지 출력을 위한 문자열 포맷
+                    string msg = string.Format("\n<color=#00ff00>{0}</color> is killed by <color=#ff0000>{1}</color>", photonView.Owner.NickName, lastShootPlayer.NickName);
+                    photonView.RPC("KillMessage", RpcTarget.AllBufferedViaServer, msg);
+                }
                 StartCoroutine(PlayerDie());
             }
         }
     }
 
+    [PunRPC]
+    void KillMessage(string msg)
+    {
+        // 메시지 출력
+        gameManager.msgList.text += msg;
+    }
     IEnumerator PlayerDie()
     {
         // CharacterController 컴포넌트 비활성화
