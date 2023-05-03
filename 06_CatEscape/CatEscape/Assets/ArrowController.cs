4ea5bc7515fb14184203d0f7723af305c4a87c69
kimkeon827 <111333313+kimkeon827@users.noreply.github.com>
2023-05-03 23:09:41 +0900

    0503 - 3
    화살 피하기 게임. 고양이 translate, 화살 generator, 화살 충돌 구현

---
+++ 06_CatEscape/CatEscape/Assets/ArrowController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    GameObject player;
+    
+    private void Start()
+    {
+        this.player = GameObject.Find("player");
+    }
+
+    void Update()
+    {
+        transform.Translate(0, -0.1f, 0);   // 프레임마다 등속으로 낙하
+
+        if (transform.position.y < -5.0f)   // 땅보다 아래로 떨어지면
+        {
+            Destroy(gameObject);       // 이 오브젝트롤 파괴
+        }
+
+        // 충돌 판정
+        Vector2 p1 = transform.position;                // 화살의 중심 좌표
+        Vector2 p2 = this.player.transform.position;    // 플레이어의 중심 좌표
+        Vector2 dir = p1 - p2;
+        float d = dir.magnitude;
+        float r1 = 0.5f;                                // 화살의 반경
+        float r2 = 1.0f;                                // 플레이어의 반경
+
+        if (d < r1 + r2)            // 충돌하면
+        {
+            Destroy(gameObject);    // 화살 삭제
+        }
+    }
+}
---
+++ 06_CatEscape/CatEscape/Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        // 왼쪽 화살표가 눌렸을 때
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-3, 0, 0);  // 왼쪽으로 3 이동
+        }
+
+        // 오른쪽 화살표가 눌렸을 때
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(3, 0, 0);   // 오른쪽으로 3 이동
+        }
+    }
+}
